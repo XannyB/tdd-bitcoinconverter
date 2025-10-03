442eb5aee304e5a0f529116f61ea7be5fbf5c422
XannyB <xannyb@gmail.com>
2025-10-03 12:42:07 +0100

    2nd test

--- src/main/Convertor.py
+++ src/main/Convertor.py
@@ -2,3 +2,5 @@ class Convertor():
     def get_exchange_rate(self, currency):
         if currency == "USD":
             return 100
+        if currency == "GBR":
+            return 200
--- src/test/test_Convertor.py
+++ src/test/test_Convertor.py
@@ -1,12 +1,20 @@
 from src.main.Convertor import Convertor
 
-def test_get_exchange_rate_USD():
-    # arrange
-    convertor = Convertor()
+# arrange
+convertor = Convertor()
 
+def test_get_exchange_rate_USD():
     # act
     actual = convertor.get_exchange_rate("USD")
 
     # assert
     expected = 100
+    assert (actual == expected)
+
+def test_get_exchange_rate_GBR():
+    # act
+    actual = convertor.get_exchange_rate("GBR")
+
+    # assert
+    expected = 200
     assert (actual == expected)
