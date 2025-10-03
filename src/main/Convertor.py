344c5c10b3c03c8d59957dc69d4ad32db0d1c6b5
XannyB <xannyb@gmail.com>
2025-10-03 13:38:11 +0100

    simple convert function, rounds off first simple version

--- src/main/Convertor.py
+++ src/main/Convertor.py
@@ -4,3 +4,14 @@ class Convertor():
             return 100
         if currency == "GBP":
             return 200
+        if currency == "EUR":
+            return 300
+    
+    def convert_bitcoin(self, currency, coins):
+        num_currency = 0;
+
+        exchange_rate = self.get_exchange_rate(currency)
+
+        num_currency = exchange_rate * coins
+
+        return num_currency
--- src/test/test_Convertor.py
+++ src/test/test_Convertor.py
@@ -21,3 +21,59 @@ def test_get_exchange_rate_GBP(convertor):
     # assert
     expected = 200
     assert actual == expected
+
+def test_get_exchange_rate_EUR(convertor):
+    # act
+    actual = convertor.get_exchange_rate("EUR")
+
+    # assert
+    expected = 300
+    assert actual == expected
+
+def test_convert_bitcoin_to_USD(convertor):
+    # act
+    actual = convertor.convert_bitcoin("USD", 1)
+
+    # assert
+    expected = 100
+    assert actual == expected
+
+def test_convert_two_bitcoin_to_USD(convertor):
+    # act
+    actual = convertor.convert_bitcoin("USD", 2)
+
+    # assert
+    expected = 200
+    assert actual == expected
+
+def test_convert_bitcoin_to_GBP(convertor):
+    # act
+    actual = convertor.convert_bitcoin("GBP", 1)
+
+    # assert
+    expected = 200
+    assert actual == expected
+
+def test_convert_two_bitcoin_to_GBP(convertor):
+    # act
+    actual = convertor.convert_bitcoin("GBP", 2)
+
+    # assert
+    expected = 400
+    assert actual == expected
+
+def test_convert_bitcoin_to_EUR(convertor):
+    # act
+    actual = convertor.convert_bitcoin("EUR", 1)
+
+    # assert
+    expected = 300
+    assert actual == expected
+
+def test_convert_two_bitcoin_to_EUR(convertor):
+    # act
+    actual = convertor.convert_bitcoin("EUR", 2)
+
+    # assert
+    expected = 600
+    assert actual == expected
